8b8815f4ba0bcf8ebd8e929b12e8d1f3275a511d
apervmm <apervmm@gmail.com>
2026-03-05 01:36:22 -0800

    feat: get and post routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
+import { matchRouter } from './routes/matches.js';
 
 const app = express();
 const PORT = 8000;
 
+// enables Middleware to parse JSON bodies in requests
 app.use(express.json());
 
 app.get('/', (_, res) => {
-  res.json({ message: 'SportsEngine server is running!' });
+  res.send({ message: 'SportsEngine server is running!' });
 });
+
+app.use('/matches', matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
---
+++ src/routes/matches.js
@@ -0,0 +1,64 @@
+import { Router } from 'express';
+import { createMatchSchema, listMatchesQuerySchema } from '../validation/matches.js';
+import { matches } from '../db/schema.js';
+import { db } from '../db/db.js';
+import { getMatchStatus } from '../utils/match-status.js';
+import { desc } from 'drizzle-orm';
+
+
+const MAX_LIMIT = 100;
+
+
+export const matchRouter = Router();
+
+matchRouter.get('/', async (req, res) => {
+
+
+    const parsed = listMatchesQuerySchema.safeParse(req.query);
+
+    if (!parsed.success) {
+        return res.status(400).json({ error: "Invalid Query Parameters", details: JSON.stringify(parsed.error.errors) });
+    }
+
+    const limit = Math.min(parsed.data.limit || 50, MAX_LIMIT);
+
+
+    try{
+        const data = await db
+            .select()
+            .from(matches)
+            .orderBy(desc(matches.createdAt))
+            .limit(limit);
+        res.status(200).json({ data });
+    } catch (error) {  
+        res.status(500).json({ error: 'Failed to fetch matches', details: JSON.stringify(error) });
+    }
+});
+
+
+matchRouter.post('/', async (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body);
+
+    if (!parsed.success) {
+        return res.status(400).json({ error: "Invalid Payload", details: JSON.stringify(parsed.error.errors) });
+    }
+
+    const { startTime, endTime, homeScore, awayScore } = parsed.data;
+
+    try{
+        const [event] =  await db.insert(matches).values({
+            ...parsed.data,
+            startTime: new Date(startTime),
+            endTime: new Date(endTime),
+            homeScore: homeScore ?? 0,
+            awayScore: awayScore ?? 0,
+            status: getMatchStatus(startTime, endTime),
+        }).returning();
+
+        res.status(201).json({ data: event });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to create match', details: JSON.stringify(error) });   
+    }
+
+    // res.status(201).json({ message: 'Create a new match' });
+});
